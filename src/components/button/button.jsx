4db68fe16f5ce1e4a52d5510f438f661091b971a
Aung Kaung Myat <aungkaungmyat2596@gmail.com>
2021-08-17 13:24:07 +0630

    redirect error fixed

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -1,19 +1,16 @@
 import React from 'react';
 
-import { motion } from 'framer-motion';
-
 import './button.scss';
 
-const Button = ({ children, small, round, route, childVariants, ...otherProps }) => (
-    <motion.a
-        variants={childVariants}
+const Button = ({ children, small, round, route, ...otherProps }) => (
+    <a
         href={route ? route : null}
         className={`
     ${small ? 'button-small' : ''}
     ${round ? 'button-round' : ''} 
     button`} {...otherProps}>
         {children}
-    </motion.a>
+    </a>
 )
 
 export default Button;
